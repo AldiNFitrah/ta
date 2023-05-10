4fcfd1b5d700aca2f00337fb890b8446f6eb346c
AldiNFitrah <aldifitrahx@gmail.com>
2023-05-10 17:12:14 +0700

    Create classifier

---
+++ src/classifier/classifier.py
@@ -0,0 +1,9 @@
+from random import random
+
+
+class Classifier:
+    def get_hate_speech_relativity_score(self, text):
+        return random * 100
+
+
+classifier = Classifier()
---
+++ src/classifier/startup_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install dependencies
+rm -rf /var/lib/man-db/auto-update
+apt-get -qq update
+apt-get install -y unzip python3-pip python3
+
+# Clone repository
+curl -L -o app.zip https://github.com/AldiNFitrah/ta/archive/refs/heads/main.zip
+unzip app -d /opt
+rm -rf app.zip
+
+cd /opt/ta-main
+sudo chown -R $USER:$USER ./
+
+# Install requirements
+pip3 install -r requirements.txt
+
+# Start application
+nohup python3 main.py classify
