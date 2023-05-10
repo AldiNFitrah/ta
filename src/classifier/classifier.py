a2da9e5e265f3c9e1ec2b52e9a0e305730f44478
AldiNFitrah <aldifitrahx@gmail.com>
2023-05-10 20:27:54 +0700

    Fix error in classifier

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -3,7 +3,7 @@ from random import random
 
 class Classifier:
     def get_hate_speech_relativity_score(self, text):
-        return random * 100
+        return random() * 100
 
 
 classifier = Classifier()
