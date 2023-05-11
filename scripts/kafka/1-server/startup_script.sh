2766e36a917770d83516e25955012fc00cae4051
AldiNFitrah <aldifitrahx@gmail.com>
2023-05-11 13:47:01 +0700

    Change log.dirs in kafka config

--- scripts/kafka/1-server/startup_script.sh
+++ scripts/kafka/1-server/startup_script.sh
@@ -21,11 +21,10 @@ echo "External IP Address: $EXTERNAL_IP"
 
 
 echo "Configuring Kafka"
-sudo mkdir /var/lib/kafka
-sudo mkdir /var/lib/kafka/logs
-sudo chown -R $USER:$USER /var/lib/kafka
+sudo mkdir /var/log/kafka
+sudo chown -R $USER:$USER /var/log/kafka
 
-sudo sed -i 's|log.dirs=.*|log.dirs=/var/lib/kafka/logs|g' config/kraft/server.properties
+sudo sed -i 's|log.dirs=.*|log.dirs=/var/log/kafka|g' config/kraft/server.properties
 sudo sed -i "s|advertised.listeners=.*|advertised.listeners=PLAINTEXT://$EXTERNAL_IP:9092|g" config/kraft/server.properties
 
 KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
