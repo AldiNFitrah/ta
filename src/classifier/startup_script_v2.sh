5c7d7d2fc509407fdf8a1f5ab4a71e45858565c2
AldiNFitrah <aldifitrahx@gmail.com>
2023-05-23 15:15:12 +0700

    Create new startup script for classifier and preprocessor

---
+++ src/classifier/startup_script_v2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install dependencies
+sudo rm -rf /var/lib/man-db/auto-update
+sudo apt-get -qq update
+sudo apt-get install -y python3-pip python3 git
+
+# Clone repository
+sudo mkdir /opt/ta
+cd /opt/ta
+sudo git clone https://github.com/AldiNFitrah/ta.git .
+sudo git pull origin main
+
+sudo chown -R $USER:$USER ./
+
+# Install requirements
+pip3 install -r requirements.txt
+
+# Start application
+nohup python3 -m src.classifier.main
--- src/preprocessor/startup_script.sh
+++ src/preprocessor/startup_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install dependencies
-rm -rf /var/lib/man-db/auto-update
-apt-get -qq update
-apt-get install -y unzip python3-pip python3
+sudo rm -rf /var/lib/man-db/auto-update
+sudo apt-get -qq update
+sudo apt-get install -y unzip python3-pip python3
 
 # Clone repository
-curl -L -o app.zip https://github.com/AldiNFitrah/ta/archive/refs/heads/main.zip
-unzip app -d /opt
-rm -rf app.zip
+sudo curl -L -o app.zip https://github.com/AldiNFitrah/ta/archive/refs/heads/main.zip
+sudo unzip app -d /opt
+sudo rm -rf app.zip
 
 cd /opt/ta-main
 sudo chown -R $USER:$USER ./
