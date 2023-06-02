86505393a3dd3e74d1d4fa62670f30dd6c7d5a67
AldiNFitrah <aldifitrahx@gmail.com>
2023-06-03 02:29:22 +0700

    Add utils to measure running time

---
+++ src/utils/measure.py
@@ -0,0 +1,15 @@
+import timeit
+from functools import wraps
+
+def measure_execution_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = timeit.default_timer()
+        result = func(*args, **kwargs)
+        end_time = timeit.default_timer()
+
+        execution_time = end_time - start_time
+        print(f"Execution Time of {func.__name__}: {execution_time} seconds")
+        return result
+
+    return wrapper
